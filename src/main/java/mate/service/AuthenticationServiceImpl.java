59998c12d55c619fcec8f435914bf3c1eacc55a7
Maxim Moshenskiy <osidiv@ukr.net>
2021-10-20 17:41:21 +0300

    added logger to the project

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -1,19 +1,24 @@
 package mate.service;
 
 import java.util.Optional;
+
 import mate.exception.AuthenticationException;
 import mate.lib.Inject;
 import mate.lib.Service;
 import mate.model.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
     @Inject
     private DriverService driverService;
 
     @Override
     public Driver login(String login, String password)
             throws AuthenticationException {
+        logger.info("login method was called. Params: login = {}", login);
         Optional<Driver> optionalDriver = driverService.findByLogin(login);
         if (optionalDriver.isPresent()
                 && optionalDriver.get().getPassword().equals(password)) {
