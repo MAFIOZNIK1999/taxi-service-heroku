b03f39c0f2462ffc36fc41597ded87ebf5feb6f2
Maxim Moshenskiy <osidiv@ukr.net>
2021-10-21 11:34:25 +0300

    init commit

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,11 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi?"
-            + "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode"
-            + "=false&serverTimezone=UTC";
-    private static final String USERNAME = "YOUR NAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
+    private static final String URL = "jdbc:mysql://remotemysql.com:3306/RGQA3n2SXt";
+    private static final String USERNAME = "RGQA3n2SXt";
+    private static final String PASSWORD = "qxLDNEClvX";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
